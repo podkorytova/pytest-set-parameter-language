d03a3690b0e6ec6a3668136360af3dcdcb12dc76
Dmitry Podkorytov <dmitry.podkorytov@gmail.com>
2021-01-07 22:50:30 +0100

    Add assert

--- test_items.py
+++ test_items.py
@@ -1,8 +1,9 @@
 import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_button_is_available(browser):
     browser.get(link)
     #time.sleep(10)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), "Button is not available"
 

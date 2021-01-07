55cf903fc28282c7a2fba6f68efa10b89f632e10
Dmitry Podkorytov <dmitry.podkorytov@gmail.com>
2021-01-07 22:13:08 +0100

    Task implementation https://stepik.org/lesson/237240/step/9?unit=209628

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_button_is_available(browser):
+    browser.get(link)
+    #time.sleep(10)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
+
